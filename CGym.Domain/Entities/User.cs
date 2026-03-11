b3c9e59bef50d9233c13f74c53d9efdfa8367e4d
Leon020785 <leonlorenzen85@gmail.com>
2026-03-11 12:41:27 +0100

    AUTH: Create User entity in Domain layer Added User entity with Id, Username, Email, PasswordHash and CreatedAt

---
+++ CGym.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CGym.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; private set; } // EF Core kan sætte værdien, andre klasser kan ikke ændre den.
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; } // vi bruger Hash for sikkerhed her.
+
+        public DateTime CreatedAt { get; set;}
+    }
+}
