7d3d58ffef380d3b27eb91accc856ec3466cba51
Leon020785 <leonlorenzen85@gmail.com>
2026-03-12 22:33:02 +0100

    Implement JWT authentication system
    - User registration endpoint
    - Login endpoint
    - Password hashing with BCrypt
    - JWT token generation
    - JWT authentication middleware
    - Protected API endpoints with [Authorize]
    - MySQL user storage
    - Scalar API testing

---
+++ CGym.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CGym.Application.Services;
+using CGym.API.DTOs;
+
+namespace CGym.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            await _authService.RegisterUserAsync(
+                request.Username,
+                request.Email,
+                request.Password
+            );
+
+            return Ok("User registered successfully");
+        }
+        [HttpPost("Login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+
+            var user = await _authService.LoginUserAsync(
+                request.Email,
+                request.Password
+                );
+            if (user == null)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            var token = _authService.GenerateJwtToken(user);
+
+            return Ok(new { token });
+
+        }
+    }
+}
---
+++ CGym.API/DTOs/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CGym.API.DTOs
+{
+    public class RegisterRequest
+    {
+        public string Username {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
