700983861db995fda048e5c89f6dd220cead6c2e
Leon020785 <leonlorenzen85@gmail.com>
2026-03-14 22:58:12 +0100

    "Create repository interfaces for Activity, Trainer, Booking (#2)"

---
+++ CGym.Application/Interfaces/IActivityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CGym.Domain.Entities;
+
+namespace CGym.Application.Interfaces
+{
+    public interface IActivityRepository
+    {
+        Task<IEnumerable<Activity>> GetAllAsync(); // hent alle aktiviteter fra databsen
+        Task<Activity?> GetByIdAsync(int id); // aktivitet basert på id.
+        Task AddAsync(Activity activity); 
+        Task DeleteAsync(int id);
+
+
+
+    }
+}
---
+++ CGym.Application/Interfaces/IBookingRepository.cs
@@ -0,0 +1,21 @@
+﻿using CGym.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CGym.Application.Interfaces
+{
+    public interface IBookingRepository
+    {
+        Task<IEnumerable<Booking>> GetAllAsync();
+        // Henter alle trænere fra databasen
+
+        Task<Booking?> GetByIdAsync(int id);
+        // Henter én træner baseret på ID
+
+        Task AddAsync(Booking booking);
+        Task DeleteAsync(int id);
+
+    }
+}
