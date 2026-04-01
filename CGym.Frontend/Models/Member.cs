1a1924dc601c8616c7900252edbc063cde039363
Leon020785 <leonlorenzen85@gmail.com>
2026-04-01 15:58:37 +0200

    Implemented login UI and connected to API

---
+++ CGym.Frontend/Models/Member.cs
@@ -0,0 +1,11 @@
+﻿namespace CGym.Frontend.Models
+{
+    public class Member
+    {
+        public int Id { get; set; } // id fra db
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ CGym.Frontend/Services/MemberService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using CGym.Frontend.Models;
+
+namespace CGym.Frontend.Services
+{
+    public class MemberService
+    {
+        private readonly HttpClient _http;
+
+        public MemberService(IHttpClientFactory factory)
+        {
+            _http = factory.CreateClient("API");
+        }
+
+        public async Task<List<Member>> GetMembersAsync()
+        {
+            return await _http.GetFromJsonAsync<List<Member>>("api/members");
+        }
+    }
+}
