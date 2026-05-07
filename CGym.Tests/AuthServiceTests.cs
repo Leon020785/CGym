0ff7b10f40786ca7e81b15cae1f744ef244a7219
Leon020785 <leonlorenzen85@gmail.com>
2026-05-07 12:44:28 +0200

    feat: add auth service tests with BCrypt password hashing

---
+++ CGym.Tests/AuthServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using BCrypt.Net;
+
+namespace CGym.Tests
+{
+    public class AuthServiceTests
+    {
+        [Fact]
+        public void Password_ShouldBeHashed()
+        {
+            var password = "MinAdgangskode123";
+            var hash = BCrypt.Net.BCrypt.HashPassword(password);
+            Assert.NotEqual(password, hash);
+        }
+
+        [Fact]
+        public void Password_ShouldVerifyCorrectly()
+        {
+            var password = "MinAdgangskode123";
+            var hash = BCrypt.Net.BCrypt.HashPassword(password);
+            var result = BCrypt.Net.BCrypt.Verify(password, hash);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WrongPassword_ShouldFailVerification()
+        {
+            var password = "MinAdgangskode123";
+            var hash = BCrypt.Net.BCrypt.HashPassword(password);
+            var result = BCrypt.Net.BCrypt.Verify("ForkertPassword", hash);
+            Assert.False(result);
+        }
+    }
+}
